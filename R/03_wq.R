430001f123fc0aced3f6db3085f8a3c79d789d32
Shannon Dunnigan <Shannon.Dunnigan@dep.state.fl.us>
2020-01-23 17:44:33 -0500

    starting point for wq

--- R/03_wq.R
+++ R/03_wq.R
@@ -7,12 +7,25 @@ df_wq %>%
 
 
 
-  ggplot(aes(x = datetimestamp, y = sal)
-         
-# summaries
+#   ggplot(aes(x = datetimestamp, y = sal)
+#          
+# # summaries
+# df_wq %>%
+#   dplyr::mutate(date = lubridate::date(datetimestamp),
+#                 month = lubridate::month(date),
+#                 day = lubridate::day(date)) %>%
+#   dplyr::group_by(station_name, month)
+#   # dplyr::summarise_all(list(~min(.), ~max(.))) %>%
+#   
+#   
+# df_wq %>%
+#   filter(station_name == "gtmsswq" & year == 2018) %>%
+#   ggplot(aes(x = datetimestamp, y = sal)) +
+#   geom_point()
+
 df_wq %>%
-  dplyr::mutate(date = lubridate::date(datetimestamp),
-                month = lubridate::month(date),
-                day = lubridate::day(date)) %>%
-  dplyr::group_by(station_name, month) %>%
-  # dplyr::summarise_all(list(~min(.), ~max(.))) %>%
+  filter(year != 2019) %>%
+  select(-datetimestamp, -hour, -day) %>%
+  group_by(station_name, month) %>%
+  summarise_all(~mean(., na.rm = TRUE)) %>%
+  mutate(`2005-2018` = "2005-2018")
