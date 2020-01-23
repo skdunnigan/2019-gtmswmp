e5c3e04df19722ab00b1f214df3a38470001240e
Shannon Dunnigan <Shannon.Dunnigan@dep.state.fl.us>
2020-01-23 17:44:25 -0500

    updated to include 2005-2019 data.

--- R/01.2_load_wrangle_wq.R
+++ R/01.2_load_wrangle_wq.R
@@ -1,35 +1,43 @@
 # ----02 LOAD water quality data ------------------------------------------------------
-df_wq_pi <- SWMPr::import_local(here::here('data', '837846.zip'), 'gtmpiwq')
-df_wq_ss <- SWMPr::import_local(here::here('data', '837846.zip'), 'gtmsswq')
-df_wq_fm <- SWMPr::import_local(here::here('data', '837846.zip'), 'gtmfmwq')
-df_wq_pc <- SWMPr::import_local(here::here('data', '837846.zip'), 'gtmpcwq')
+df_wq_pi <- SWMPr::import_local(here::here('data', '13394.zip'), 'gtmpiwq')
+df_wq_ss <- SWMPr::import_local(here::here('data', '13394.zip'), 'gtmsswq')
+df_wq_fm <- SWMPr::import_local(here::here('data', '13394.zip'), 'gtmfmwq')
+df_wq_pc <- SWMPr::import_local(here::here('data', '13394.zip'), 'gtmpcwq')
 
 # qaqc and remove bad values
 # add column that has station name in each df
 df_wq_pi <- df_wq_pi %>%
-  SWMPr::qaqc(qaqc_keep = c('0', '1', '5')) %>%
+  SWMPr::qaqc(qaqc_keep = c('0', '1', '4', '5')) %>%
   dplyr::mutate(station_name = 'gtmpiwq')
 df_wq_ss <- df_wq_ss %>%
-  SWMPr::qaqc(qaqc_keep = c('0', '1', '5')) %>%
+  SWMPr::qaqc(qaqc_keep = c('0', '1', '4', '5')) %>%
   dplyr::mutate(station_name = 'gtmsswq')
 df_wq_fm <- df_wq_fm %>%
-  SWMPr::qaqc(qaqc_keep = c('0', '1', '5')) %>%
+  SWMPr::qaqc(qaqc_keep = c('0', '1', '4', '5')) %>%
   dplyr::mutate(station_name = 'gtmfmwq')
 df_wq_pc <- df_wq_pc %>%
-  SWMPr::qaqc(qaqc_keep = c('0', '1', '5')) %>%
+  SWMPr::qaqc(qaqc_keep = c('0', '1', '4', '5')) %>%
   dplyr::mutate(station_name = 'gtmpcwq')
 
 # merge all the files into one dataframe
-df_wq <- dplyr::bind_rows(df_wq_pi, df_wq_ss, df_wq_fm, df_wq_pc)
+# make sure station_name is ordered
+# pull out year, month, day, hour 
+df_wq <- dplyr::bind_rows(df_wq_pi, 
+                          df_wq_ss, 
+                          df_wq_fm, 
+                          df_wq_pc) %>%
+  dplyr::mutate(station_name = factor(station_name,
+                                      levels = c("gtmpiwq",
+                                                 "gtmsswq",
+                                                 "gtmfmwq",
+                                                 "gtmpcwq")),
+                year = lubridate::year(as.Date(datetimestamp)),
+                month = lubridate::month(as.Date(datetimestamp)),
+                day = lubridate::day(as.Date(datetimestamp)),
+                hour = lubridate::hour(datetimestamp)) %>%
+  dplyr::select(-level, -clevel)
 
-# remove individual stations df
+# remove individual stations df ---- cleanup
 rm(df_wq_pi, df_wq_ss, df_wq_fm, df_wq_pc)
 
-# make sure station_name is ordered
-df_wq$station_name <- as.factor(df_wq$station_name)
-df_wq$station_name <- factor(df_wq$station_name, 
-                             levels = c("gtmpiwq",
-                                        "gtmsswq",
-                                        "gtmfmwq",
-                                        "gtmpcwq")
-)
+beep(11) # sound alert
